9effc93ff00fc954bf168a8c37eb0d266ca488b2
Matthew Ackard <mattackard@gmail.com>
2018-07-24 16:45:53 -0700

    added space around text. got rid of navigation.

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+
 //get window height and width
 
 let windowHeight = window.screen.height;
@@ -8,9 +9,6 @@ let offset = 0;                                  //initial offset for background
 let imgBreakpoint = 800;                         //width in pixels when image changes to wide version
 let orientation = screen.msOrientation || (screen.orientation || screen.mozOrientation || {}).type;   //get orientation of screen for mobile
 
-const $nav = $('nav');
-const $navAbout = $('#navAbout');
-const $navPortfolio = $('#navPortfolio');
 const $about = $('#about');
 const $portfolio = $('.project');
 
@@ -18,23 +16,16 @@ function bgImageCenter() {
   if ($('#bgImg').width() -  window.innerWidth > 0) {
     offset = ($('#bgImg').width() -  window.innerWidth) / 2 * -1;
     $('#bgImg').css('right', offset + 'px');
-    console.log(offset);
   }
 }
 
-function adjustWide() {
+function adjustWide() {                                       //changes the background image source for wide(landscape) and thin(portrait) screens
   orientation = screen.msOrientation || (screen.orientation || screen.mozOrientation || {}).type;
   if (windowWidth > imgBreakpoint || orientation === 'landscape-primary') {
     $("#bgImg").attr("src", "img/bg_image_wide.jpg");
-    $nav.hide();                    //hide nav and show about and portfolio
-    $about.show();
-    $portfolio.show();
   }
   else {
     $("#bgImg").attr("src", "img/bg_image_thin.jpg");
-    $portfolio.hide();         //start with partfolio hidden
-    $nav.show();
-    $about.show();
   }
 }
 
@@ -46,15 +37,4 @@ $(window).resize(() => {                                            //adjusts th
   windowWidth = window.screen.width;
   adjustWide();
   bgImageCenter();
-});
-
-$('nav').click((e) => {                                       //fade in selected content and hide the unselected content
-  if ($(e.target).text() == 'About') {
-    $portfolio.hide();
-    $about.fadeIn();
-  }
-  if ($(e.target).text() == 'Portfolio') {
-    $about.hide();
-    $portfolio.fadeIn();
-  }
 });
